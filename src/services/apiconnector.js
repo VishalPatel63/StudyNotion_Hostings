b34e45c4b78266e6e889b99cc4c8f7427c1a3498
Vishal patel <155652463+VishalPatel63@users.noreply.github.com>
2024-08-25 20:59:44 +0530

    Update apiconnector.js

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -2,25 +2,17 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({});
 
-// export const apiConnector = (method, url, bodyData, headers, params) => {
-//     return axiosInstance({
-//         method:`${method}`,
-//         url:`${url}`,
-//         data: bodyData ? bodyData : null,
-//         headers: headers ? headers: null,
-//         params: params ? params : null,
-//     });
-// }
+export const apiConnector = (method, url, bodyData, headers, params) => {
+    return axiosInstance({
+        method:`${method}`,
+        url:`${url}`,
+        data: bodyData ? bodyData : null,
+        headers: headers ? headers: null,
+        params: params ? params : null,
+    });
+}
 
-export const apiConnector = (method, url, bodyData = null, headers = {}, params = {}) => {
-  return axiosInstance({
-    method,  // method is directly used
-    url,     // url is directly used
-    data: bodyData,
-    headers, // default value is empty object
-    params,  // default value is empty object
-  });
-};
+
 
 
 
