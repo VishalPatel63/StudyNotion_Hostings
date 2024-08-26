66364abbef62d7149f0f186489ccbaf5e9626d0c
Vishal patel <155652463+VishalPatel63@users.noreply.github.com>
2024-08-26 15:05:17 +0530

    Update index.jsx

--- src/components/core/Dashboard/AddCourse/index.jsx
+++ src/components/core/Dashboard/AddCourse/index.jsx
@@ -3,7 +3,7 @@ import  RenderSteps  from './RenderSteps'
 
 export default function AddCourse(){
   return (
-    <div>
+       <div className='overflow-x-hidden'>
         <div className='flex w-full items-start gap-x-6 '>
             <div className='flex flex-1 flex-col '>
                 <h1 className='mb-14 lg:text-3xl md:text-3xl text-2xl  font-medium text-richblack-5 lg:mt-0 md:mt-0 mt-10'>Add Course</h1>
