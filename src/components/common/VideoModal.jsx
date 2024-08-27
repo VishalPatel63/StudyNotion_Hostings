997cf57580e3cbcc3edd886ee1d172ce565c8f97
Vishal patel <155652463+VishalPatel63@users.noreply.github.com>
2024-08-28 00:46:58 +0530

    Update VideoModal.jsx

--- src/components/common/VideoModal.jsx
+++ src/components/common/VideoModal.jsx
@@ -140,7 +140,7 @@ export const VideoModal = ({setReviewModal,setModal}) => {
         <div className=' flex justify-center '>
        <button  
        onClick={()=> setModal(false)}
-       className='mt-6 h-10 w-20 text-sm text-center  px-3 bg-yellow-100 rounded-md'
+       className='mt-6 h-10 w-20 text-sm text-center px-3 bg-yellow-100 rounded-md'
        >
        <MdOutlineArrowBackIos  size={30} className='text-center ml-3 text-black' />
        </button>
