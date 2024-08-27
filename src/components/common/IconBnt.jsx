4ca8308ac7c3e2da4b256938f06ccc3be7d037d7
Vishal patel <155652463+VishalPatel63@users.noreply.github.com>
2024-08-27 21:59:06 +0530

    Update IconBnt.jsx

--- src/components/common/IconBnt.jsx
+++ src/components/common/IconBnt.jsx
@@ -15,7 +15,7 @@ export const IconBnt = ({
      onClick={onclick}
      type={type}
      className= {`flex items-center ${outline ? "border border-yellow-50 bg-transparent" :"bg-yellow-50"}
-     cursor-pointer gap-x-2 rounded-md lg:py-2 md:py-2  lg:px-5 md:px-5 px-2 py-1 lg:text-base md:text-base text-sm  font-semibold text-richblack-900 ${customClasses}
+     cursor-pointer gap-x-2 rounded-md lg:py-2 md:py-2  lg:px-5 md:px-5 px-2 py-2 lg:text-base md:text-base text-sm  font-semibold text-richblack-900 ${customClasses}
      `}
      >
        {children ? (
